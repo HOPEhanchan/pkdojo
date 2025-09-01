998b2f8cc848043c883004344272648a6695757f
半田望 <_hope_@handanoomunoAir.lan>
2025-09-01 22:24:58 +0900

    feat(game): scaffold機能を用いてGame model（user association and routes）を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_31_150615) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_01_131704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.boolean "result"
+    t.string "choice"
+    t.string "keeper_choice"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +36,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_31_150615) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "games", "users"
 end
