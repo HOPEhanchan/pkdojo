3197135ac8f4ee2d305f617560b1bb6aeee372ec
半田望 <_hope_@handanoomunoAir.lan>
2025-09-20 04:41:12 +0900

    feat(stats)基本的な戦績画面の実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,6 +3,14 @@ class GamesController < ApplicationController
 
   def index
     @games = current_user.games.order(created_at: :desc)
+
+    total = @games.size
+    goals = @games.where(result: true).count
+    @stats = {
+      total: total,
+      goals: goals,
+      rate:  total.zero? ? 0 : ((goals.to_f / total) * 100).round(1)
+    }
   end
 
   def shoot
@@ -15,6 +23,23 @@ class GamesController < ApplicationController
     @result = result
     render :result
   end
+
+  def stats
+  @games = current_user.games.order(created_at: :desc)
+
+  total = @games.size
+  goals = @games.where(result: true).count
+  saves = @games.where(result: false).count
+  streak = @games.reverse.take_while(&:result).count # 連続ゴール数の表示
+
+  @stats = {
+    total: total,
+    goals: goals,
+    saves: saves,
+    rate: total.zero? ? 0 : ((goals.to_f / total) * 100).round(1),
+    streak: streak
+  }
+end
 
   private
   #def set_game
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :games, only: [:index] do
+  resources :games, only: [:index, :show] do
     post :shoot, on: :collection
+    collection do
+      get :stats # /games/stats で戦績ページへ
+    end
   end
   root "games#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
