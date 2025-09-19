21a32f1f32129525d6432a6ca2aee41ee1abc6e4
半田望 <_hope_@handanoomunoAir.lan>
2025-09-19 23:03:06 +0900

    UIの基本設定: PK field と 3 clickable zones と shoot flow

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,70 +1,30 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: %i[ show edit update destroy ]
+  #before_action :set_game, only: %i[ show ]
 
-  # GET /games or /games.json
   def index
-    @games = Game.all
+    @games = current_user.games.order(created_at: :desc)
   end
 
-  # GET /games/1 or /games/1.json
-  def show
-  end
+  def shoot
+    user_choice   = params[:choice]                 # 選択肢は３択で　"left"|"center"|"right"
+    keeper_choice = %w[left center right].sample
+    result        = (user_choice != keeper_choice)  # GKと違う方向ならゴール
 
-  # GET /games/new
-  def new
-    @game = Game.new
-  end
+    @game = current_user.games.create!(
+      result: result, choice: user_choice, keeper_choice: keeper_choice
+    )
 
-  # GET /games/1/edit
-  def edit
-  end
-
-  # POST /games or /games.json
-  def create
-    @game = Game.new(game_params)
-
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: "Game was successfully created." }
-        format.json { render :show, status: :created, location: @game }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /games/1 or /games/1.json
-  def update
-    respond_to do |format|
-      if @game.update(game_params)
-        format.html { redirect_to @game, notice: "Game was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @game }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /games/1 or /games/1.json
-  def destroy
-    @game.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to games_path, notice: "Game was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    @keeper_choice = keeper_choice
+    @result = result
+    render :result
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Game.find(params[:id])
-    end
+  #def set_game
+  #  @game = Game.find(params[:id])
+  #end
 
-    # Only allow a list of trusted parameters through.
-    def game_params
-      params.require(:game).permit(:user_id, :result, :choice, :keeper_choice)
-    end
+  def game_params
+    params.require(:game).permit(:result, :choice, :keeper_choice)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :games do
+  resources :games, only: [:index] do
     post :shoot, on: :collection
   end
   root "games#index"
