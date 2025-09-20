b0d232f86a9ec5b201f3e34d4728bfdd220470b0
半田望 <_hope_@handanoomunoAir.lan>
2025-09-20 20:19:14 +0900

    fix_ルーティングやコントローラーの微調整

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
-  #before_action :set_game, only: %i[ show ]
+  before_action :authenticate_user!
+  before_action :set_game, only: [:show]
 
   def index
     @games = current_user.games.order(created_at: :desc)
@@ -11,6 +12,10 @@ class GamesController < ApplicationController
       goals: goals,
       rate:  total.zero? ? 0 : ((goals.to_f / total) * 100).round(1)
     }
+  end
+
+  def show
+    # @game は set_game で取得済み
   end
 
   def shoot
@@ -46,9 +51,10 @@ class GamesController < ApplicationController
 end
 
   private
-  #def set_game
-  #  @game = Game.find(params[:id])
-  #end
+
+  def set_game
+    @game = current_user.games.find(params[:id])
+  end
 
   def game_params
     params.require(:game).permit(:result, :choice, :keeper_choice)
