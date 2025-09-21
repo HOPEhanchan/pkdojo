6d288310d186d2a1f1a1b2a35d628e5b8d14c234
半田望 <_hope_@handanoomunoAir.lan>
2025-09-21 11:10:59 +0900

    デプロイの調整中_config/routesの調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,21 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
+  #ログイン済みの人のroot
+  authenticated: user do
+    root "game#index", as: :authenticated_root
+  end
+
+  #未ログインの人のroot（まずはゲームTOP。）
+  unauthenticated do
+    root "game#index"
+  end
+
   resources :games, only: [:index, :show] do
     post :shoot, on: :collection
     collection { get :stats } # /games/stats で戦績ページへ
   end
+
   root "games#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
